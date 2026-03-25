064e735f2cb1c599bf2032c0a6387b212201e0aa
ththfql <145094239+ththfql@users.noreply.github.com>
2026-03-25 14:03:02 +0900

    dada

---
+++ week3/３－３.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<string> words;
+	vector<int> counts ;
+
+	string input;
+	int found = 0;
+
+	cout << "입력 할 단어들을 쓰세요: " << endl;
+
+	while (cin >> input) {
+
+	}
+
+	return 0;
+}
